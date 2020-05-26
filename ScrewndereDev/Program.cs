4b1f7b9a4d6d6a8e08946c630b36d3ae499b0ab4
Nikita Krapivin <alienoom@yandex.ru>
2020-05-27 00:05:53 +0500

    Добавьте файлы проекта.

---
+++ ScrewndereDev/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DiscordRPC;
+
+namespace ScrewndereDev
+{
+    class Program
+    {
+        private const string DISCORD_CLIENT_ID = "560185502691491841"; // the magic value that was totally randomly generated...
+        private const string PRESENCE_STATUS = "Awaiting Verification"; // oh and I totally got this by bruteforcing too...
+
+        // A little wrapper around Console.WriteLine so I wouldn't need to type Console..... each time.
+        private static void Writeline(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        private static void Main()
+        {
+            // If you see this text, that means your .NET Framework installation is not screwed up...
+            Writeline("ScrewndereDev V1.0 - Now with 0% Spaghetti code!");
+            Writeline("Initializing...");
+
+            // Initialize a new RPC Client.
+            DiscordRpcClient discord = new DiscordRpcClient(DISCORD_CLIENT_ID);
+
+            // Define Callbacks.
+            discord.OnReady += (sender, e) => Writeline(string.Format("Received Ready from user: {0}#{1}", e.User.Username, e.User.Discriminator));
+            discord.OnPresenceUpdate += (sender, e) => Writeline(string.Format("Received new presence: {0}" + Environment.NewLine + "Now go type -verifyme", e.Presence.State));
+            discord.OnRpcMessage += (sender, e) => Writeline(string.Format("Recieved RPC call: {0}", e.Type.ToString()));
+            discord.OnJoinRequested += (sender, e) => Writeline(string.Format("Join request: {0}#{1}", e.User.Username, e.User.Discriminator));
+            discord.OnError += (sender, e) => Writeline(string.Format("Error: {0}", e.Message));
+            discord.OnSubscribe += (sender, e) => Writeline(string.Format("OnSubscribe: {0} : {1}", e.Event.ToString(), e.Type.ToString()));
+            discord.OnConnectionEstablished += (sender, e) => Writeline(string.Format("Connection established (Pipe Ind): {0}", e.ConnectedPipe.ToString()));
+
+            // Actually ask the library to do something (and print initialization's result).
+            Writeline("Initialize() result: " + (discord.Initialize() ? "OK! :)" : "Error! :("));
+
+            // Bypass the check
+            discord.SetPresence(new RichPresence() { State = PRESENCE_STATUS });
+
+            // And then we loop forever processing Discord's requests.
+            while (true) discord.Invoke();
+        }
+    }
+}
---
+++ ScrewndereDev/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ScrewndereDev")]
+[assembly: AssemblyDescription("The ultimate tool for your screwing needs!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nkrapivindev developments. From Russia with <3")]
+[assembly: AssemblyProduct("ScrewndereDev")]
+[assembly: AssemblyCopyright("Copyright ©  2020+ literally no one lol")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c6489ed3-c5e9-4f5b-9fee-cefea633dbc5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
